1f309712f8b15fbae6f880aaafc46948efe3a6bc
shanliangLS <986747978@qq.com>
2019-04-14 15:07:54 +0800

    完成底部导航栏

--- app/src/main/java/hehut/scse/kaoyanbang/MainActivity.java
+++ app/src/main/java/hehut/scse/kaoyanbang/MainActivity.java
@@ -3,14 +3,31 @@ package hehut.scse.kaoyanbang;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hehut.scse.kaoyanbang.TabFragment.TabFragment1;
+import hehut.scse.kaoyanbang.TabFragment.TabFragment2;
+import hehut.scse.kaoyanbang.TabFragment.TabFragment3;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    // ViewPager
+    private ViewPager mViewPager;
+    // 适配器
+    private FragmentPagerAdapter mAdapter;
+    // 装载fragment
+    private List<Fragment> mFragments;
+    // 底部导航栏
+    private BottomNavigationView navigation;
 
+    // 底部导航栏监听
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,13 +35,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mViewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mViewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mViewPager.setCurrentItem(2);
                     return true;
             }
             return false;
@@ -36,9 +53,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mViewPager = findViewById(R.id.viewpager);
+        navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initTabFragment();
+    }
+
+    // 初始化Fragment适配器
+    private void initTabFragment() {
+        mFragments = new ArrayList<>();
+        mFragments.add(new TabFragment1());
+        mFragments.add(new TabFragment2());
+        mFragments.add(new TabFragment3());
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                mViewPager.setCurrentItem(position);
+                navigation.setSelectedItemId(navigation.getMenu().getItem(position).getItemId());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
     }
 
 }
---
+++ app/src/main/java/hehut/scse/kaoyanbang/TabFragment/TabFragment2.java
@@ -0,0 +1,23 @@
+package hehut.scse.kaoyanbang.TabFragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hehut.scse.kaoyanbang.R;
+
+public class TabFragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tabfragment2, container, false);
+
+
+
+        return view;
+    }
+}
